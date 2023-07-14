0a071c3684d93f88c66e423bb0262774bfdf45a6
duiqt <mrfuc@yahoo.com>
2023-07-14 17:36:38 +0700

    update

---
+++ src/pages/Teacher/reportCard/ReportCardTeacherPage.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect, useCallback } from "react";
+import GridWrapper from "../../../components/common/GridWrapper/GridWrapper";
+import client from "../../../api/client";
+import { Paper, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import FolderIcon from "@mui/icons-material/Folder";
+const ReportCardTeacherPage = () => {
+	const [loading, setLoading] = useState(true);
+	const [selectedClass, setSelectedClass] = useState(null);
+	const [classScores, setClassScores] = useState([]);
+	const colors = [
+		"rgba(76, 175, 80, 0.15)",
+		"rgba(255, 87, 34, 0.15)",
+		"rgba(3, 169, 244, 0.15)",
+		"rgba(156, 39, 176, 0.15)",
+		"rgba(255, 193, 7, 0.15)",
+		"rgba(233, 30, 99, 0.15)",
+	];
+	const numClasses = classScores.length;
+	const navigate = useNavigate();
+
+	const fetchDataStudents = useCallback(async (classId) => {
+		try {
+			const response = await client.get(
+				`/api/student/classes/${classId}/students`
+			);
+			const students = response.data;
+			setClassScores((prevClassScores) => {
+				const updatedClassScores = prevClassScores.map((classItem) => {
+					if (classItem.id === classId) {
+						return {
+							...classItem,
+							numStudents: students.length,
+						};
+					}
+					return classItem;
+				});
+				return updatedClassScores;
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	}, []);
+
+	const [teacherId, setTeacherId] = useState("");
+
+	useEffect(() => {
+		const storedId = localStorage.getItem("id");
+		setTeacherId(storedId);
+	}, [localStorage.getItem("id")]);
+
+	useEffect(() => {
+		const fetchClassData = async () => {
+			try {
+				const response = await client.get(`/api/teachers/${teacherId}/classes`);
+				const classData = response.data;
+				setClassScores(classData);
+				classData.forEach((classItem) => {
+					fetchDataStudents(classItem.id);
+				});
+				setLoading(false);
+			} catch (error) {
+				console.error(error);
+				setLoading(false);
+			}
+		};
+
+		if (teacherId) {
+			fetchClassData();
+		}
+	}, [teacherId, fetchDataStudents]);
+
+	const handleClassClick = (classId) => {
+		setSelectedClass(classId);
+		navigate(`/teacher/class-reportCards/${classId}`);
+	};
+
+	return (
+		<GridWrapper>
+			<Typography variant="h6" sx={{ marginBottom: "10px" }}>
+				Tổng số lớp: {numClasses}
+			</Typography>
+			<Paper
+				elevation={3}
+				sx={{
+					padding: "20px",
+					display: "flex",
+					flexWrap: "wrap",
+					justifyContent: "space-around",
+				}}
+			>
+				{classScores.map((classItem, index) => (
+					<Paper
+						key={classItem.id}
+						elevation={3}
+						sx={{
+							cursor: "pointer",
+							marginBottom: "10px",
+							padding: "10px",
+							width: "calc(33% - 10px)",
+							boxSizing: "border-box",
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "center",
+							flexDirection: "column",
+							backgroundColor: colors[index % colors.length],
+							color: "#616161",
+						}}
+						onClick={() => handleClassClick(classItem.id)}
+					>
+						<FolderIcon fontSize="large" />
+						<Typography variant="h6">{classItem.name}</Typography>
+						<Typography variant="subtitle1">
+							Số học sinh: {classItem.numStudents || 0}
+						</Typography>
+					</Paper>
+				))}
+			</Paper>
+		</GridWrapper>
+	);
+};
+
+export default ReportCardTeacherPage;
