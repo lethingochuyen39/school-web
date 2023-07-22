0521f031086bf1cb820b8b73d7697723d88099fd
QuanLe0702 <quanle0702@gmail.com>
2023-07-23 00:28:25 +0700

    update

--- src/pages/Subject/SubjectTeacherPage.js
+++ src/pages/Subject/SubjectTeacherPage.js
@@ -1,6 +1,5 @@
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import {
-<<<<<<< HEAD
   Button,
   Dialog,
   DialogContent,
@@ -12,14 +11,6 @@ import {
   Modal,
   Select,
   Typography,
-=======
-	Button,
-	FormControl,
-	IconButton,
-	InputLabel,
-	MenuItem,
-	Select,
->>>>>>> b6908a760ebdc352ece6f83afe082fc5d3f2ca47
 } from "@mui/material";
 import Box from "@mui/material/Box";
 import React, { useEffect, useState } from "react";
@@ -32,7 +23,6 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function SubjectTeacherPage() {
-<<<<<<< HEAD
   const { id } = useParams();
   const [teachers, setTeachers] = useState([]);
   const [selectTeachers, setSelectTeachers] = useState([]);
@@ -40,48 +30,40 @@ export default function SubjectTeacherPage() {
   const navigate = useNavigate();
   const [isViewModalOpen, setIsViewModalOpen] = useState(false);
   const [selectedTeacherDetails, setSelectedTeacherDetails] = useState(null);
-=======
-	const { id } = useParams();
-	const [teachers, setTeachers] = useState([]);
-	const [selectTeachers, setSelectTeachers] = useState([]);
-	const [selectedTeacher, setSelectedTeacher] = useState("");
-	const navigate = useNavigate();
->>>>>>> b6908a760ebdc352ece6f83afe082fc5d3f2ca47
 
-	const fetchTeacherData = async () => {
-		try {
-			const response = await client.get(
-				`/api/teachers/subjects/${id}/teachers`
-			);
-			const teachersResponse = await client.get("/api/teachers");
-			const activeTeachers = teachersResponse.data.filter(
-				(teacher) => teacher.isActive
-			);
-			setSelectTeachers(activeTeachers);
-			setSelectedTeacher(activeTeachers[0]?.id || "");
-			setTeachers(response.data);
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  const fetchTeacherData = async () => {
+    try {
+      const response = await client.get(
+        `/api/teachers/subjects/${id}/teachers`
+      );
+      const teachersResponse = await client.get("/api/teachers");
+      const activeTeachers = teachersResponse.data.filter(
+        (teacher) => teacher.isActive
+      );
+      setSelectTeachers(activeTeachers);
+      setSelectedTeacher(activeTeachers[0]?.id || "");
+      setTeachers(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	useEffect(() => {
-		fetchTeacherData();
-	}, [id]);
+  useEffect(() => {
+    fetchTeacherData();
+  }, [id]);
 
-	const handleTeacherChange = (event) => {
-		const id = event.target.value;
-		setSelectedTeacher(id);
-		console.log(selectedTeacher);
-	};
+  const handleTeacherChange = (event) => {
+    const id = event.target.value;
+    setSelectedTeacher(id);
+    console.log(selectedTeacher);
+  };
 
-	const handleSubmit = () => {
-		const data = {
-			teacherId: selectedTeacher,
-			subjectId: id,
-		};
+  const handleSubmit = () => {
+    const data = {
+      teacherId: selectedTeacher,
+      subjectId: id,
+    };
 
-<<<<<<< HEAD
     // Check if the selected teacher is already in the teachers state
     if (teachers.find((teacher) => teacher.id === data.teacherId)) {
       toast.warning("Giáo viên đã được thêm vào môn học.");
@@ -125,122 +107,94 @@ export default function SubjectTeacherPage() {
       console.error(error);
     }
   };
-=======
-		client
-			.post(`/api/subjects/${data.subjectId}/teachers/${data.teacherId}`)
-			.then((response) => {
-				fetchTeacherData();
-				toast.success("Thêm giáo viên vào môn học thành công");
-			})
-			.catch((error) => {
-				console.error("Lỗi khi thêm:", error.response.data);
-			});
-	};
 
-	const handleDelete = async (teacherId) => {
-		const data = {
-			teacherId: teacherId,
-			subjectId: id,
-		};
-		try {
-			await client.delete(
-				`/api/subjects/${data.subjectId}/teachers/${data.teacherId}`
-			);
-			fetchTeacherData();
-		} catch (error) {
-			console.error(error);
-		}
-	};
->>>>>>> b6908a760ebdc352ece6f83afe082fc5d3f2ca47
+  const handleGoBack = () => {
+    navigate(-1);
+  };
 
-	const handleGoBack = () => {
-		navigate(-1);
-	};
+  const getHeader = () => (
+    <>
+      <IconButton onClick={handleGoBack}>
+        <ArrowBackIcon />
+      </IconButton>
 
-	const getHeader = () => (
-		<>
-			<IconButton onClick={handleGoBack}>
-				<ArrowBackIcon />
-			</IconButton>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        padding="5px"
+        flexWrap="wrap"
+      >
+        <FormControl fullWidth margin="normal" size="small">
+          <InputLabel id="teacher-label">Chọn giáo viên</InputLabel>
+          <Select
+            labelId="teacher-label"
+            id="teacher-select"
+            name="teacherId"
+            value={selectedTeacher}
+            onChange={handleTeacherChange}
+            label="Chọn giáo viên"
+            sx={{ height: "40px" }}
+          >
+            {selectTeachers.map((teacher) => (
+              <MenuItem key={teacher.id} value={teacher.id}>
+                {teacher.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+    </>
+  );
 
-			<Box
-				display="flex"
-				justifyContent="space-between"
-				alignItems="center"
-				padding="5px"
-				flexWrap="wrap"
-			>
-				<FormControl fullWidth margin="normal" size="small">
-					<InputLabel id="teacher-label">Chọn giáo viên</InputLabel>
-					<Select
-						labelId="teacher-label"
-						id="teacher-select"
-						name="teacherId"
-						value={selectedTeacher}
-						onChange={handleTeacherChange}
-						label="Chọn giáo viên"
-						sx={{ height: "40px" }}
-					>
-						{selectTeachers.map((teacher) => (
-							<MenuItem key={teacher.id} value={teacher.id}>
-								{teacher.name}
-							</MenuItem>
-						))}
-					</Select>
-				</FormControl>
-			</Box>
-		</>
-	);
+  const columns = [
+    { field: "id", headerName: "ID", width: 100, disableActions: true },
+    {
+      field: "name",
+      headerName: "Giáo viên",
+      width: 150,
+      disableActions: true,
+    },
+  ];
 
-	const columns = [
-		{ field: "id", headerName: "ID", width: 100, disableActions: true },
-		{
-			field: "name",
-			headerName: "Giáo viên",
-			width: 150,
-			disableActions: true,
-		},
-	];
+  const getContent = () => (
+    <>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        fontSize="2rem"
+        fontWeight="bold"
+        sx={{
+          fontWeight: "bold",
+          textShadow: "1px 1px 2px rgba(0, 0, 0, 0.3)",
+          color: "#FF4500",
+          textAlign: "center",
+        }}
+      >
+        Nhập giáo viên
+      </Box>
 
-	const getContent = () => (
-		<>
-			<Box
-				display="flex"
-				justifyContent="center"
-				alignItems="center"
-				fontSize="2rem"
-				fontWeight="bold"
-				sx={{
-					fontWeight: "bold",
-					textShadow: "1px 1px 2px rgba(0, 0, 0, 0.3)",
-					color: "#FF4500",
-					textAlign: "center",
-				}}
-			>
-				Nhập giáo viên
-			</Box>
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+        alignItems="center"
+        padding="5px"
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSubmit}
+          style={{
+            background: "linear-gradient(45deg, #304ffe, #42a5f5)",
+            fontSize: "1.2rem",
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          Lưu
+        </Button>
+      </Box>
 
-			<Box
-				display="flex"
-				justifyContent="flex-end"
-				alignItems="center"
-				padding="5px"
-			>
-				<Button
-					variant="contained"
-					color="primary"
-					onClick={handleSubmit}
-					style={{
-						background: "linear-gradient(45deg, #304ffe, #42a5f5)",
-						fontSize: "1.2rem",
-						boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-					}}
-				>
-					Lưu
-				</Button>
-			</Box>
-
-<<<<<<< HEAD
       <DataTable
         initialRows={teachers}
         columns={columns}
@@ -343,5 +297,4 @@ export default function SubjectTeacherPage() {
       <BasicCard header={getHeader()} content={getContent()} />
     </GridWrapper>
   );
-e082fc5d3f2ca47
 }
