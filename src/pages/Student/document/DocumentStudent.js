d98808b3b586ecf7857517bfce120e9d2162ceea
DESKTOP-AHSGN44\Ngoc Huyen <huyenle021039@gmail.com>
2023-07-22 23:39:10 +0700

    update huyen

--- src/pages/Student/document/DocumentStudent.js
+++ src/pages/Student/document/DocumentStudent.js
@@ -8,6 +8,7 @@ import {
 	Box,
 	Divider,
 	Input,
+	LinearProgress,
 	Pagination,
 	Paper,
 	Typography,
@@ -29,7 +30,21 @@ export default function DocumentStudentPage() {
 	const [documents, setDocument] = useState([]);
 	const [searchTerm, setSearchTerm] = useState("");
 	const [noResults, setNoResults] = useState(false);
+	const [student, setStudent] = useState(null);
 
+	useEffect(() => {
+		const fetchStudentData = async () => {
+			try {
+				const studentId = localStorage.getItem("id");
+				const response = await client.get(`/api/student/${studentId}`);
+				setStudent(response.data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		fetchStudentData();
+	}, []);
 	const fetchData = useCallback(async () => {
 		try {
 			let url = "/api/documents";
@@ -78,159 +93,174 @@ export default function DocumentStudentPage() {
 		fetchData();
 	}, [fetchData]);
 	return (
-		<ThemeProvider theme={defaultTheme}>
-			<CssBaseline />
-			<Container maxWidth="lg">
-				<main>
-					<Grid container spacing={5} paddingTop={2}>
-						<Grid
-							item
-							xs={12}
-							md={9}
-							sx={{
-								py: 3,
-							}}
-						>
-							<Typography
-								variant="h6"
-								sx={{ fontWeight: "600", color: "#1a237e" }}
-								gutterBottom
-							>
-								THAM KHẢO TÀI LIỆU
-							</Typography>
-							<Divider />
-							{noResults ? (
-								<Typography
-									variant="subtitle1"
-									sx={{ textAlign: "center", marginTop: 2, color: "#bf360c" }}
-								>
-									Không tìm thấy kết quả phù hợp
-								</Typography>
-							) : (
-								<>
-									<Grid container spacing={4} marginTop={0.5}>
-										{documents.map((documents) => (
-											<Grid item xs={12} md={6} key={documents.id}>
-												<CardActionArea component="a">
-													<Card
-														sx={{
-															display: "flex",
-															height: 170,
-															border: 0.05,
-														}}
-													>
-														<CardContent sx={{ flex: 1 }}>
-															<Typography
-																variant="subtitle1"
-																sx={{
-																	fontWeight: 600,
-																	lineHeight: "1.2rem",
-																	height: "2.5rem",
-																	overflow: "hidden",
-																}}
-															>
-																{documents.title}
-															</Typography>
-															<Typography
-																variant="body2"
-																color="text.secondary"
-																sx={{
-																	fontSize: "0.7rem",
-																}}
-															>
-																{documents.formattedDateTime}
-															</Typography>
-															<Typography
-																variant="body1"
-																sx={{
-																	overflow: "hidden",
-																	height: "3.5rem",
-																	lineHeight: "1.2rem",
-																}}
-															>
-																{documents.description}
-															</Typography>
-
-															<FileDownloader
-																url={`/api/documents/${documents.id}/download`}
-																fileName={documents.fileName}
-															/>
-														</CardContent>
-														<CardMedia
-															component="img"
-															src={
-																documents.fileName.endsWith(".doc") ||
-																documents.fileName.endsWith(".docx")
-																	? "/images/icons/word_icon.svg"
-																	: documents.fileName.endsWith(".pdf")
-																	? "/images/icons/pdf_icon.png"
-																	: documents.fileName.endsWith(".xlsx")
-																	? "/images/icons/excel.png"
-																	: ""
-															}
-															alt={documents.title}
-															sx={{
-																width: 70,
-																display: { xs: "none", sm: "block" },
-															}}
-														/>
-													</Card>
-												</CardActionArea>
-											</Grid>
-										))}
-									</Grid>
-									<Pagination
-										count={totalPages}
-										page={currentPage}
-										onChange={handleChangePage}
-										sx={{ mt: 4, justifyContent: "center" }}
-									/>
-								</>
-							)}
-						</Grid>
-						<Grid item xs={12} md={3}>
-							<Paper elevation={0} sx={{ p: 2, bgcolor: "grey.200" }}>
-								<Typography variant="h6" gutterBottom>
-									Xin chào !
-								</Typography>
-								<Typography>
-									Tìm tài liệu một cách nhanh chóng và tiện lợi tại đây.
-								</Typography>
-							</Paper>
-							<Typography
-								variant="h6"
-								gutterBottom
-								sx={{ fontWeight: "600", color: "#1a237e", mt: 3 }}
-							>
-								Tìm kiếm
-							</Typography>
-							<Box
-								minWidth={{ xs: "100%", sm: 0, md: "100px" }}
-								marginRight={{ xs: 0, sm: "10px" }}
-								marginBottom={{ xs: "10px", sm: 0 }}
-								backgroundColor="#f5f5f5"
-								borderRadius="4px"
-								padding="4px"
-								display="flex"
-								alignItems="center"
-							>
-								<SearchIcon sx={{ marginRight: "15px" }} />
-								<Input
-									placeholder="Tìm kiếm theo tiêu đề...  "
-									onChange={handleSearchChange}
-									value={searchTerm}
+		<>
+			{student === null ? (
+				<LinearProgress />
+			) : student && !(student.status === "active") ? (
+				<div style={{ fontWeight: "bold", color: "#1565c0" }}>
+					Tài khoản cá nhân bạn đang bị khóa. Vui lòng liên hệ nhà trường để
+					biết thêm thông tin.
+				</div>
+			) : (
+				<ThemeProvider theme={defaultTheme}>
+					<CssBaseline />
+					<Container maxWidth="lg">
+						<main>
+							<Grid container spacing={5} paddingTop={2}>
+								<Grid
+									item
+									xs={12}
+									md={9}
 									sx={{
-										width: { xs: "100%", sm: "auto", md: "100%" },
-										color: "rgba(0, 0, 0, 0.6)",
-										fontSize: "1.1rem",
+										py: 3,
 									}}
-									disableUnderline
-								/>
-							</Box>
-						</Grid>
-					</Grid>
-				</main>
-			</Container>
-		</ThemeProvider>
+								>
+									<Typography
+										variant="h6"
+										sx={{ fontWeight: "600", color: "#1a237e" }}
+										gutterBottom
+									>
+										THAM KHẢO TÀI LIỆU
+									</Typography>
+									<Divider />
+									{noResults ? (
+										<Typography
+											variant="subtitle1"
+											sx={{
+												textAlign: "center",
+												marginTop: 2,
+												color: "#bf360c",
+											}}
+										>
+											Không tìm thấy kết quả phù hợp
+										</Typography>
+									) : (
+										<>
+											<Grid container spacing={4} marginTop={0.5}>
+												{documents.map((documents) => (
+													<Grid item xs={12} md={6} key={documents.id}>
+														<CardActionArea component="a">
+															<Card
+																sx={{
+																	display: "flex",
+																	height: 170,
+																	border: 0.05,
+																}}
+															>
+																<CardContent sx={{ flex: 1 }}>
+																	<Typography
+																		variant="subtitle1"
+																		sx={{
+																			fontWeight: 600,
+																			lineHeight: "1.2rem",
+																			height: "2.5rem",
+																			overflow: "hidden",
+																		}}
+																	>
+																		{documents.title}
+																	</Typography>
+																	<Typography
+																		variant="body2"
+																		color="text.secondary"
+																		sx={{
+																			fontSize: "0.7rem",
+																		}}
+																	>
+																		{documents.formattedDateTime}
+																	</Typography>
+																	<Typography
+																		variant="body1"
+																		sx={{
+																			overflow: "hidden",
+																			height: "3.5rem",
+																			lineHeight: "1.2rem",
+																		}}
+																	>
+																		{documents.description}
+																	</Typography>
+
+																	<FileDownloader
+																		url={`/api/documents/${documents.id}/download`}
+																		fileName={documents.fileName}
+																	/>
+																</CardContent>
+																<CardMedia
+																	component="img"
+																	src={
+																		documents.fileName.endsWith(".doc") ||
+																		documents.fileName.endsWith(".docx")
+																			? "/images/icons/word_icon.svg"
+																			: documents.fileName.endsWith(".pdf")
+																			? "/images/icons/pdf_icon.png"
+																			: documents.fileName.endsWith(".xlsx")
+																			? "/images/icons/excel.png"
+																			: ""
+																	}
+																	alt={documents.title}
+																	sx={{
+																		width: 70,
+																		display: { xs: "none", sm: "block" },
+																	}}
+																/>
+															</Card>
+														</CardActionArea>
+													</Grid>
+												))}
+											</Grid>
+											<Pagination
+												count={totalPages}
+												page={currentPage}
+												onChange={handleChangePage}
+												sx={{ mt: 4, justifyContent: "center" }}
+											/>
+										</>
+									)}
+								</Grid>
+								<Grid item xs={12} md={3}>
+									<Paper elevation={0} sx={{ p: 2, bgcolor: "grey.200" }}>
+										<Typography variant="h6" gutterBottom>
+											Xin chào !
+										</Typography>
+										<Typography>
+											Tìm tài liệu một cách nhanh chóng và tiện lợi tại đây.
+										</Typography>
+									</Paper>
+									<Typography
+										variant="h6"
+										gutterBottom
+										sx={{ fontWeight: "600", color: "#1a237e", mt: 3 }}
+									>
+										Tìm kiếm
+									</Typography>
+									<Box
+										minWidth={{ xs: "100%", sm: 0, md: "100px" }}
+										marginRight={{ xs: 0, sm: "10px" }}
+										marginBottom={{ xs: "10px", sm: 0 }}
+										backgroundColor="#f5f5f5"
+										borderRadius="4px"
+										padding="4px"
+										display="flex"
+										alignItems="center"
+									>
+										<SearchIcon sx={{ marginRight: "15px" }} />
+										<Input
+											placeholder="Tìm kiếm theo tiêu đề...  "
+											onChange={handleSearchChange}
+											value={searchTerm}
+											sx={{
+												width: { xs: "100%", sm: "auto", md: "100%" },
+												color: "rgba(0, 0, 0, 0.6)",
+												fontSize: "1.1rem",
+											}}
+											disableUnderline
+										/>
+									</Box>
+								</Grid>
+							</Grid>
+						</main>
+					</Container>
+				</ThemeProvider>
+			)}
+		</>
 	);
 }
--- src/pages/Student/schedule/StudentScheduleView.js
+++ src/pages/Student/schedule/StudentScheduleView.js
@@ -9,13 +9,13 @@ import {
 	Paper,
 	Box,
 	Typography,
-	TextField,
 	Button,
 	Grid,
 	FormControl,
 	InputLabel,
 	Select,
 	MenuItem,
+	LinearProgress,
 } from "@mui/material";
 import client from "../../../api/client";
 import * as XLSX from "xlsx";
@@ -28,7 +28,20 @@ const StudentScheduleView = () => {
 	const [selectedClass, setSelectedClass] = useState("");
 
 	const [isInitialDataLoaded, setIsInitialDataLoaded] = useState(false);
+	const [student, setStudent] = useState(null);
+	useEffect(() => {
+		const fetchStudentData = async () => {
+			try {
+				const studentId = localStorage.getItem("id");
+				const response = await client.get(`/api/student/${studentId}`);
+				setStudent(response.data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
 
+		fetchStudentData();
+	}, []);
 	const handleChange = (event) => {
 		setSelectedClass(event.target.value);
 	};
@@ -164,139 +177,155 @@ const StudentScheduleView = () => {
 
 	return (
 		<>
-			<Box>
-				<Typography
-					variant="h4"
-					sx={{
-						mb: 2,
-						fontWeight: "bold",
-						textShadow: "1px 1px 2px rgba(0, 0, 0, 0.3)",
-						color: "#FF4500",
-						textAlign: "center",
-					}}
-				>
-					Thời Khóa Biểu
-				</Typography>
-			</Box>
-			<Grid container justifyContent="center" spacing={2} sx={{ mb: 2, mt: 2 }}>
-				<Grid item xs={12} md={6}>
-					<FormControl fullWidth size="small">
-						<InputLabel id="class-label">Lớp học</InputLabel>
-						<Select
-							labelId="class-label"
-							id="class-select"
-							name="classId"
-							size="small"
-							value={selectedClass}
-							onChange={handleChange}
-							label="Lớp học"
-							required
-						>
-							{classes.map((classItem) => (
-								<MenuItem key={classItem.id} value={classItem.id}>
-									LH{classItem.id}_{classItem.name}_năm học:{" "}
-									{classItem.academicYear.name}; Áp dụng TKB từ{" "}
-									{classItem.academicYear.startDate} đến{" "}
-									{classItem.academicYear.endDate}
-								</MenuItem>
-							))}
-						</Select>
-					</FormControl>
-				</Grid>
-
-				<Grid item xs={12} md={6}>
-					<Box mb={2} sx={{ display: "flex", justifyContent: "flex-end" }}>
-						<Button
-							variant="contained"
+			{student === null ? (
+				<LinearProgress />
+			) : student && !(student.status === "active") ? (
+				<div style={{ fontWeight: "bold", color: "#1565c0" }}>
+					Tài khoản cá nhân bạn đang bị khóa. Vui lòng liên hệ nhà trường để
+					biết thêm thông tin.
+				</div>
+			) : (
+				<>
+					<Box>
+						<Typography
+							variant="h4"
 							sx={{
+								mb: 2,
 								fontWeight: "bold",
-								color: "white",
-								backgroundImage: "linear-gradient(to right, #8bc34a, #4caf50)",
+								textShadow: "1px 1px 2px rgba(0, 0, 0, 0.3)",
+								color: "#FF4500",
+								textAlign: "center",
 							}}
-							onClick={handleExportExcel}
 						>
-							Export Excel
-						</Button>
+							Thời Khóa Biểu
+						</Typography>
 					</Box>
-				</Grid>
-			</Grid>
+					<Grid
+						container
+						justifyContent="center"
+						spacing={2}
+						sx={{ mb: 2, mt: 2 }}
+					>
+						<Grid item xs={12} md={6}>
+							<FormControl fullWidth size="small">
+								<InputLabel id="class-label">Lớp học</InputLabel>
+								<Select
+									labelId="class-label"
+									id="class-select"
+									name="classId"
+									size="small"
+									value={selectedClass}
+									onChange={handleChange}
+									label="Lớp học"
+									required
+								>
+									{classes.map((classItem) => (
+										<MenuItem key={classItem.id} value={classItem.id}>
+											LH{classItem.id}_{classItem.name}_năm học:{" "}
+											{classItem.academicYear.name}; Áp dụng TKB từ{" "}
+											{classItem.academicYear.startDate} đến{" "}
+											{classItem.academicYear.endDate}
+										</MenuItem>
+									))}
+								</Select>
+							</FormControl>
+						</Grid>
 
-			<TableContainer component={Paper}>
-				<Table sx={{ minWidth: 650 }} aria-label="simple table">
-					<TableHead>
-						<TableRow>
-							<TableCell
-								sx={{
-									fontWeight: "bold",
-									backgroundColor: "#0097a7",
-									color: "#FFFFFF",
-									textAlign: "center",
-									border: "1px solid black",
-								}}
-							>
-								Tiết
-							</TableCell>
-							{dayOfWeekData.map((d) => (
-								<TableCell
-									key={d.id}
-									align="center"
+						<Grid item xs={12} md={6}>
+							<Box mb={2} sx={{ display: "flex", justifyContent: "flex-end" }}>
+								<Button
+									variant="contained"
 									sx={{
 										fontWeight: "bold",
-										color: "#FFFFFF",
-										backgroundColor: "#0097a7",
-										textAlign: "center",
-										border: "1px solid black",
+										color: "white",
+										backgroundImage:
+											"linear-gradient(to right, #8bc34a, #4caf50)",
 									}}
+									onClick={handleExportExcel}
 								>
-									{d.name}
-								</TableCell>
-							))}
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{scheduleData.map((row, i) => (
-							<TableRow
-								key={i}
-								sx={{
-									"&:nth-of-type(odd)": {
-										backgroundColor: "#e3f2fd",
-									},
-								}}
-							>
-								<TableCell
-									sx={{
-										fontWeight: "bold",
-										width: "100px",
-										textAlign: "center",
-										whiteSpace: "nowrap",
-										border: "1px solid black",
-									}}
-								>
-									{row.tietName}
-									<br />( {row.startTime} - {row.endTime})
-								</TableCell>
-								{dayOfWeekData.map((d) => (
+									Export Excel
+								</Button>
+							</Box>
+						</Grid>
+					</Grid>
+					<TableContainer component={Paper}>
+						<Table sx={{ minWidth: 650 }} aria-label="simple table">
+							<TableHead>
+								<TableRow>
 									<TableCell
-										key={d.id}
-										align="center"
 										sx={{
+											fontWeight: "bold",
+											backgroundColor: "#0097a7",
+											color: "#FFFFFF",
+											textAlign: "center",
 											border: "1px solid black",
 										}}
 									>
-										{row[d.id].subjectName ? (
-											<>
-												{row[d.id].subjectName} - {row[d.id].teacherName}
-											</>
-										) : (
-											"-"
-										)}
+										Tiết
 									</TableCell>
+									{dayOfWeekData.map((d) => (
+										<TableCell
+											key={d.id}
+											align="center"
+											sx={{
+												fontWeight: "bold",
+												color: "#FFFFFF",
+												backgroundColor: "#0097a7",
+												textAlign: "center",
+												border: "1px solid black",
+											}}
+										>
+											{d.name}
+										</TableCell>
+									))}
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{scheduleData.map((row, i) => (
+									<TableRow
+										key={i}
+										sx={{
+											"&:nth-of-type(odd)": {
+												backgroundColor: "#e3f2fd",
+											},
+										}}
+									>
+										<TableCell
+											sx={{
+												fontWeight: "bold",
+												width: "100px",
+												textAlign: "center",
+												whiteSpace: "nowrap",
+												border: "1px solid black",
+											}}
+										>
+											{row.tietName}
+											<br />( {row.startTime} - {row.endTime})
+										</TableCell>
+										{dayOfWeekData.map((d) => (
+											<TableCell
+												key={d.id}
+												align="center"
+												sx={{
+													border: "1px solid black",
+												}}
+											>
+												{row[d.id].subjectName ? (
+													<>
+														{row[d.id].subjectName} - {row[d.id].teacherName}
+													</>
+												) : (
+													"-"
+												)}
+											</TableCell>
+										))}
+									</TableRow>
 								))}
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</TableContainer>
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</>
+			)}
 		</>
 	);
 };
