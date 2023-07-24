5fcef183c35e201a6994bf8c5c6fdfc9a283af2d
DESKTOP-AHSGN44\Ngoc Huyen <huyenle021039@gmail.com>
2023-07-25 01:10:09 +0700

    update view score

--- src/pages/Score/ViewScorePage.js
+++ src/pages/Score/ViewScorePage.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import DataTable from "../../components/common/DataTable/DataTable";
 import client from "../../api/client";
 import Box from "@mui/material/Box";
 import GridWrapper from "../../components/common/GridWrapper/GridWrapper";
@@ -21,16 +20,19 @@ import {
 	InputLabel,
 	Select,
 	MenuItem,
+	Grid,
 } from "@mui/material";
-
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 const ViewScorePage = () => {
-	const [students, setStudents] = useState([]);
+	const [selectedStudent, setSelectedStudent] = useState("");
 	const { classId } = useParams();
 	const [subjects, setSubjects] = useState([]);
 	const [scoreTypes, setScoreTypes] = useState([]);
 	const [semester, setSemester] = useState(1);
 	const [scoreData, setScoreData] = useState([]);
 	const averageScores = {};
+	const [filteredStudents, setFilteredStudents] = useState([]);
 
 	const navigate = useNavigate();
 
@@ -43,15 +45,30 @@ const ViewScorePage = () => {
 	};
 
 	useEffect(() => {
+		if (classId) {
+			const fetchStudents = async () => {
+				try {
+					const response = await client.get(`/api/student/${classId}/students`);
+					const filteredStudents = response.data;
+					setFilteredStudents(filteredStudents);
+					console.log(filteredStudents);
+				} catch (error) {
+					console.error("Lỗi khi lấy danh sách học sinh:", error);
+				}
+			};
+
+			fetchStudents();
+		}
+	}, [classId]);
+
+	useEffect(() => {
 		const fetchScoreData = async () => {
 			try {
-				const studentId = 1;
-
 				const scoreTypeResponse = await client.get("/api/score-types");
 				const subjectResponse = await client.get("/api/subjects");
 
 				const scoreResponse = await client.get(
-					`/api/scores/semester?classId=${classId}&semester=${semester}&studentId=${studentId}`
+					`/api/scores/semester?classId=${classId}&semester=${semester}&studentId=${selectedStudent}`
 				);
 				const fetchedSubjects = subjectResponse.data.filter(
 					(subject) => !subject.name.startsWith("SHDC")
@@ -124,43 +141,54 @@ const ViewScorePage = () => {
 		};
 
 		fetchScoreData();
-	}, [classId, semester]);
+	}, [classId, semester, selectedStudent]);
+
+	const handleStudentChange = (event) => {
+		setSelectedStudent(event.target.value);
+		console.log(selectedStudent);
+	};
+
+	const handleExportExcel = () => {
+		const studentName = filteredStudents.find(
+			(student) => student.id === selectedStudent
+		)?.name;
+		const semesterName = semester === 1 ? "Học kỳ 1" : "Học kỳ 2";
+
+		const data = [
+			[semesterName, studentName],
+			["Môn học", ...scoreTypes.map((scoreType) => scoreType.name), "ĐTB"],
+			...scoreData.map((row) => [
+				row.subjectName,
+				...scoreTypes.map((scoreType) => row[scoreType.id]),
+				row.average !== null ? row.average : "-",
+			]),
+		];
+
+		// Tạo workbook mới
+		const workbook = XLSX.utils.book_new();
+
+		// Tạo worksheet từ dữ liệu
+		const worksheet = XLSX.utils.aoa_to_sheet(data);
+
+		// Thêm worksheet vào workbook
+		XLSX.utils.book_append_sheet(workbook, worksheet, "Bảng điểm");
+
+		// Xuất file Excel
+		const excelBuffer = XLSX.write(workbook, {
+			bookType: "xlsx",
+			type: "array",
+		});
+		const excelData = new Blob([excelBuffer], {
+			type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+		});
+		saveAs(excelData, "BangDiem.xlsx");
+	};
 
 	const getHeader = () => (
 		<>
 			<IconButton onClick={handleGoBack}>
 				<ArrowBackIcon />
 			</IconButton>
-
-			<Box
-				display="flex"
-				justifyContent="space-between"
-				alignItems="center"
-				padding="5px"
-				flexWrap="wrap"
-			>
-				<FormControl fullWidth margin="normal" size="small">
-					<InputLabel id="semester-label">Chọn Học kỳ</InputLabel>
-					<Select
-						labelId="semester-label"
-						id="semester-select"
-						name="semester"
-						value={semester}
-						onChange={handlesemesterChange}
-						label="Chọn Học kỳ"
-						required
-						defaultValue={1}
-					>
-						<MenuItem value={1}>Học kỳ 1</MenuItem>
-						<MenuItem value={2}>Học kỳ 2</MenuItem>
-					</Select>
-				</FormControl>
-			</Box>
-		</>
-	);
-
-	const getContent = () => (
-		<>
 			<Box
 				display="flex"
 				justifyContent="center"
@@ -176,7 +204,51 @@ const ViewScorePage = () => {
 			>
 				Xem điểm lớp
 			</Box>
+			<Grid container justifyContent="center" spacing={2}>
+				<Grid item xs={12} md={4}>
+					<FormControl fullWidth margin="normal" size="small">
+						<InputLabel id="semester-label">Học kỳ</InputLabel>
+						<Select
+							labelId="semester-label"
+							id="semester-select"
+							name="semester"
+							value={semester}
+							onChange={handlesemesterChange}
+							label="Học kỳ"
+							required
+							defaultValue={1}
+						>
+							<MenuItem value={1}>Học kỳ 1</MenuItem>
+							<MenuItem value={2}>Học kỳ 2</MenuItem>
+						</Select>
+					</FormControl>
+				</Grid>
+				<Grid item xs={12} md={4}>
+					<FormControl fullWidth size="small" margin="normal">
+						<InputLabel id="student-label">Học sinh</InputLabel>
+						<Select
+							labelId="student-label"
+							id="student-select"
+							name="studentId"
+							value={selectedStudent}
+							onChange={handleStudentChange}
+							label="Học sinh"
+							required
+						>
+							{filteredStudents.map((student) => (
+								<MenuItem key={student.id} value={student.id}>
+									HS{student.id}_{student.name}
+								</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</Grid>
+			</Grid>
+		</>
+	);
 
+	const getContent = () => (
+		<>
 			<Box
 				display="flex"
 				justifyContent="flex-end"
@@ -186,10 +258,10 @@ const ViewScorePage = () => {
 				<Button
 					variant="contained"
 					color="primary"
-					// onClick={handleExportExcel}
+					onClick={handleExportExcel}
 					style={{
 						backgroundImage: "linear-gradient(to right, #8bc34a, #4caf50)",
-						fontSize: "1.2rem",
+						fontSize: "1.1rem",
 						boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
 					}}
 				>
@@ -294,7 +366,24 @@ const ViewScorePage = () => {
 
 	return (
 		<GridWrapper>
-			<BasicCard header={getHeader()} content={getContent()} />
+			{!selectedStudent && (
+				<>
+					<BasicCard header={getHeader()} />
+					<Box
+						display="flex"
+						justifyContent="center"
+						alignItems="center"
+						height="200px"
+						fontSize="1.2rem"
+						color="#b71c1c"
+					>
+						Vui lòng chọn học sinh để tra cứu
+					</Box>
+				</>
+			)}
+			{selectedStudent && (
+				<BasicCard header={getHeader()} content={getContent()} />
+			)}
 		</GridWrapper>
 	);
 };
